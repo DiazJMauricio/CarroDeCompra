655030f972014921d217bf548e8fbbf4c85c5ee8
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2019-02-26 01:43:24 -0300

    Updates

---
+++ precache-manifest.3a378f01b0f438b5dc1d53fcb3946dd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c18d5f8fa627b876df68301c1517297",
+    "url": "/static/media/imagen1S.0c18d5f8.jpg"
+  },
+  {
+    "revision": "f7b6e8a925bf77965eaf380550e6e0e3",
+    "url": "/static/media/imagen1M.f7b6e8a9.jpg"
+  },
+  {
+    "revision": "e3cb4aa1228f3b80a124ef7bd473b598",
+    "url": "/static/media/imagen1L.e3cb4aa1.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f162bca75e364718e1f5",
+    "url": "/static/js/main.f162bca7.chunk.js"
+  },
+  {
+    "revision": "eb4a8e6f56ef01fee1fc",
+    "url": "/static/js/2.eb4a8e6f.chunk.js"
+  },
+  {
+    "revision": "f162bca75e364718e1f5",
+    "url": "/static/css/main.38a3730e.chunk.css"
+  },
+  {
+    "revision": "39195bcd5a9c19bba73e9cf2ce89a5d9",
+    "url": "/index.html"
+  }
+];
