19ee02a788136f07a0774621f49a3ac3e5ec7d1b
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2019-02-27 00:51:39 -0300

    Updates

---
+++ precache-manifest.c486a24857ea1d5de928ce9c6288048a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c18d5f8fa627b876df68301c1517297",
+    "url": "/CarroDeCompra/static/media/imagen1S.0c18d5f8.jpg"
+  },
+  {
+    "revision": "f7b6e8a925bf77965eaf380550e6e0e3",
+    "url": "/CarroDeCompra/static/media/imagen1M.f7b6e8a9.jpg"
+  },
+  {
+    "revision": "e3cb4aa1228f3b80a124ef7bd473b598",
+    "url": "/CarroDeCompra/static/media/imagen1L.e3cb4aa1.jpg"
+  },
+  {
+    "revision": "4da0c33d073d3d4f7464",
+    "url": "/CarroDeCompra/static/js/runtime~main.4da0c33d.js"
+  },
+  {
+    "revision": "6e845b0d888501ab8a7f",
+    "url": "/CarroDeCompra/static/js/main.6e845b0d.chunk.js"
+  },
+  {
+    "revision": "67db185a0e58abb5435e",
+    "url": "/CarroDeCompra/static/js/2.67db185a.chunk.js"
+  },
+  {
+    "revision": "6e845b0d888501ab8a7f",
+    "url": "/CarroDeCompra/static/css/main.f40a3487.chunk.css"
+  },
+  {
+    "revision": "5e8cbba5eeef26cff7bd7dfb621c5fee",
+    "url": "/CarroDeCompra/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.791294cf9d95d677a3c29866b932eb8c.js"
+  "/CarroDeCompra/precache-manifest.c486a24857ea1d5de928ce9c6288048a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/CarroDeCompra/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
